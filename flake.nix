6ddbf5d0ab819834434eeb7fe3d11f4b662f6427
IwnuplyNotTyan <ikissiwnuply@gmail.com>
2026-07-09 22:02:41 +0300

    feat: devshell

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "💄 Glamoured";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              go
+              gopls
+              gotools
+              golangci-lint
+            ];
+          };
+        });
+    };
+}
